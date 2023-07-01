07509c01ffc011903bfe50df5802c10fc6893773
Jose Eduardo Monteiro <josee.edumont@gmail.com>
2023-07-01 02:24:48 -0300

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use eframe::{egui, App};
+
+#[derive(Default)]
+struct ColasApp;
+
+impl App for ColasApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Head");
+            ui.label("Hello there")
+        });
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    eframe::run_native(
+        "Colas",
+        eframe::NativeOptions::default(),
+        Box::new(|_cc| Box::<ColasApp>::default()),
+    ).unwrap();
 }
